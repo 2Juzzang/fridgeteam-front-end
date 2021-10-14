4136b6d3e0d33af91912d702c68b66889eda2749
jaeiljoo <sgsgself@naver.com>
2021-10-14 18:04:01 +0900

    [update]별점 수정확인

--- frid/src/components/Star.js
+++ frid/src/components/Star.js
@@ -10,121 +10,123 @@ const Star = (props) => {
   let [check4,setCheck4] = React.useState(false)
   let [check5,setCheck5] = React.useState(false)
   
-   let a = check1 + check2 + check3 + check4 + check5;
-  let [sum,setSum] = React.useState(a)
+  let a = check1 + check2 + check3 + check4 + check5
+  let [sum, setSum] = React.useState(a)
+  console.log(a, sum)
 
-  let clickCheck1 = (check1) =>{
+  let clickCheck1 = (check1) => {
     setCheck1(!check1)
-    
+    // console.log("1", check1)
   }
-  let clickCheck2 = () => {
-      setCheck2(!check2);
-        console.log(check2);
-  };
-  let clickCheck3 = () => {
-    setCheck3(!check3);
-  };
-  let clickCheck4 = () => {
-   setCheck4(!check4)
-  };
-  let clickCheck5 = () => {
+  let clickCheck2 = (check2) => {
+    setCheck2(!check2)
+    // console.log("2", check2)
+  }
+  console.log(check2)
+  let clickCheck3 = (check3) => {
+    setCheck3(!check3)
+    // console.log("3", check3)
+  }
+  let clickCheck4 = (check4) => {
+    setCheck4(!check4)
+    // console.log("4", check4)
+  }
+  let clickCheck5 = (check5) => {
     setCheck5(!check5)
-  };
-  
-  let sumCheck = (sum) => {
-    
-     
-      setSum(a)
-       console.log(sum);
-     
+    // console.log("5", check5)
   }
+
+  let sumCheck = () => {
+    setSum(a)
+  }
+  console.log("나는버튼안에섬", sum)
   return (
-    <Grid is_flex justify_content='flex-start' padding='0px' margin='0px'>
+    <Grid is_flex justify_content="flex-start" padding="0px" margin="0px">
       <Grid
-        padding='0px'
-        width='auto'
-        margin='0px'
+        padding="0px"
+        width="auto"
+        margin="0px"
         _onClick={() => {
-          clickCheck1(check1);
+          clickCheck1(check1)
         }}
       >
         {check1 ? (
-          <FaRegGrinAlt size='70' color='red' />
+          <FaRegGrinAlt size="70" color="red" />
         ) : (
-          <FaRegGrinAlt size='70' />
+          <FaRegGrinAlt size="70" />
         )}
       </Grid>
 
       <Grid
-        padding='0px'
-        width='auto'
-        margin='0px'
+        padding="0px"
+        width="auto"
+        margin="0px"
         _onClick={() => {
-          clickCheck2(check2);
+          clickCheck2(check2)
         }}
       >
         {check2 ? (
-          <FaRegGrinAlt size='70' color='red' />
+          <FaRegGrinAlt size="70" color="red" />
         ) : (
-          <FaRegGrinAlt size='70' />
+          <FaRegGrinAlt size="70" />
         )}
       </Grid>
       <Grid
-        padding='0px'
-        width='auto'
-        margin='0px'
+        padding="0px"
+        width="auto"
+        margin="0px"
         _onClick={() => {
-          clickCheck3(check3);
+          clickCheck3(check3)
         }}
       >
         {check3 ? (
-          <FaRegGrinAlt size='70' color='red' />
+          <FaRegGrinAlt size="70" color="red" />
         ) : (
-          <FaRegGrinAlt size='70' />
+          <FaRegGrinAlt size="70" />
         )}
       </Grid>
       <Grid
-        padding='0px'
-        width='auto'
-        margin='0px'
+        padding="0px"
+        width="auto"
+        margin="0px"
         _onClick={() => {
-          clickCheck4(check4);
+          clickCheck4(check4)
         }}
       >
         {check4 ? (
-          <FaRegGrinAlt size='70' color='red' />
+          <FaRegGrinAlt size="70" color="red" />
         ) : (
-          <FaRegGrinAlt size='70' />
+          <FaRegGrinAlt size="70" />
         )}
       </Grid>
       <Grid
-        padding='0px'
-        width='auto'
-        margin='0px'
+        padding="0px"
+        width="auto"
+        margin="0px"
         _onClick={() => {
-          clickCheck5(check5);
+          clickCheck5(check5)
         }}
       >
         {check5 ? (
-          <FaRegGrinAlt size='70' color='red' />
+          <FaRegGrinAlt size="70" color="red" />
         ) : (
-          <FaRegGrinAlt size='70' />
+          <FaRegGrinAlt size="70" />
         )}
       </Grid>
 
-      <Grid padding='0px' width='auto' margin='0px 30px'>
+      <Grid padding="0px" width="auto" margin="0px 30px">
         <Button
           _onClick={() => {
-            sumCheck(sum);
+            sumCheck(a)
           }}
           is_CircleSubmitBtn
-          size='50'
+          size="50"
         >
           확인
         </Button>
       </Grid>
     </Grid>
-  );
+  )
 }
 
 export default Star;
--- frid/src/pages/Main.js
+++ frid/src/pages/Main.js
@@ -1,12 +1,10 @@
 import React from "react"
 import { BasketList } from "../components/BasketList"
 import styled from "styled-components"
-import { jsx, css, keyframes } from '@emotion/react';
 export const Main = (props) => {
   return (
     <>
       <BasketList />
-   
     </>
   )
 }
