7a9ae03d5288ec443ff69263d982583bf74e4814
JiHoon <skyy630@naver.com>
2021-10-14 23:54:21 +0900

    [edit]댓글 삭제 작업 진행중

--- frid/src/components/CommentList.js
+++ frid/src/components/CommentList.js
@@ -7,20 +7,22 @@ import { actionCreators as commentActions } from "../redux/modules/comment";
 const CommentList = (props) => {
   // 레시피의 id값 받아옴
   const { post_id } = props;
-  //리스트 가져오기
   const dispatch = useDispatch();
-
+  //리스트 가져오기
   const comment_list = useSelector((state) => state.comment.list);
+  console.log("코멘트리스트", comment_list);
+  //댓글 삭제
+  // const delComment = () => {
+
+  //   dispatch(commentActions.)
+  // }
+
+  //댓글이 0개일 때 댓글을 가져옴
   useEffect(() => {
     if (comment_list.length === 0) {
-      dispatch(commentActions.setCommentDB(comment_list));
+      dispatch(commentActions.setCommentDB());
     }
   }, []);
-  console.log("코멘리스트", comment_list);
-  const test = comment_list.filter((i) => {
-    return post_id === i.recipeTitle;
-  });
-  console.log("테스트", test);
   return (
     <>
       {comment_list
@@ -28,6 +30,8 @@ const CommentList = (props) => {
         .reverse()
         .map((c, i) => {
           return (
+            // 댓글의 데이터를 다 가져오므로
+            // 해당 포스트의 id와 가져온 데이터의 id가 일치하면 댓글을 보여준다.
             post_id === c.recipeTitle && (
               <div
                 key={i}
@@ -43,7 +47,7 @@ const CommentList = (props) => {
                   {c.content}
                 </Text>
                 <Button is_RectangleCancleBtn size="20">
-                  <Text color="white" size="10px">
+                  <Text color="white" size="10px" _onClick={() => {}}>
                     X
                   </Text>
                 </Button>
--- frid/src/redux/modules/comment.js
+++ frid/src/redux/modules/comment.js
@@ -4,6 +4,7 @@ import axios from "axios";
 //타입
 const SET_COMMENT = "SET_COMMENT";
 const ADD_COMMENT = "ADD_COMMENT";
+const DEL_COMMENT = "DEL_COMMENT";
 
 // const LOADING = "LOADING";
 
@@ -14,6 +15,9 @@ const setComment = createAction(SET_COMMENT, (comment) => ({
 const addComment = createAction(ADD_COMMENT, (comment) => ({
   comment,
 }));
+// const delComment = createAction(DEL_COMMENT, () => ({
+
+// }));
 
 // const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
@@ -22,7 +26,7 @@ const initialState = {
   //   is_loading: false,
 };
 
-//포스트 요청
+//등록
 const addCommentDB = (comment) => {
   return function (dispatch, getState, { history }) {
     axios
@@ -30,14 +34,13 @@ const addCommentDB = (comment) => {
         content: comment.commentText,
         recipeTitle: comment.post_id,
       })
-      .then((response) => {
+      .then(() => {
         dispatch(
           addComment({
             content: comment.commentText,
             recipeTitle: comment.post_id,
           })
         );
-        window.alert("등록");
       })
       .catch((error) => {
         console.log(error);
@@ -45,20 +48,24 @@ const addCommentDB = (comment) => {
   };
 };
 
-const setCommentDB = (comment) => {
+//불러오기
+const setCommentDB = () => {
   return function (dispatch, getState, { history }) {
     axios
       .get("http://3.36.72.109/api/comments")
       .then((response) => {
-        const data = response.data;
-        console.log("데이터", data);
-        dispatch(setComment(data));
+        dispatch(setComment(response.data));
       })
       .catch((error) => {
         console.log(error);
       });
   };
 };
+
+//삭제
+
+//미들웨어
+// const deleteCommentDB
 
 // handleActions를 사용하면 추가 데이터의 이름은 항상 action.payload
 export default handleActions(
@@ -84,8 +91,6 @@ export default handleActions(
 );
 
 const actionCreators = {
-  //   getCommentFB,
-  //   addComment,
   addCommentDB,
   setCommentDB,
 };
